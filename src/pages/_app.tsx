eb76977a2600a14efd3adfc6a70ba82088bc8af9
Emmanuel Haankwenda <emmanuel.haankwenda2018@gmail.com>
2024-08-30 21:25:52 +0200

    fix: export App with tRPC

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
+import { trpc } from "../utils/trpc";
 
-export default function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
       <Component {...pageProps} />
     </UserProvider>
   );
 }
+
+export default trpc.withTRPC(App);
